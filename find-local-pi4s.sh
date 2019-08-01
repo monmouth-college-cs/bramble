c4c1e330e34b93013309f9508be10e82b766944a
Robert Utterback <robertu13@gmail.com>
2019-08-01 10:15:34 -0500

    Improved nmap script to take parameters for IP prefix and Pi version

--- find-local-pi4s.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -e
-sudo nmap -n -sP 10.40.20.* | egrep -i 'dc:a6:32' -B2 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'
--- find-local-pis.sh
+++ find-local-pis.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-set -e
-sudo nmap -n -sP 10.40.20.* | egrep -i 'dc:a6:32|b8:27:eb' -B2 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'
+set -ex
+
+prefix=${1:-10.40.20} # First argument: IP address prefix
+
+# Second argument: which version Pis to detect
+macaddr=""
+if [ "$2" = "4" ]; then
+    # Just Raspbery Pi 4
+    macaddr='dc:a6:32'
+elif [ "$2" = "3" ]; then
+    # 1, 2, or 3
+    macaddr='b8:27:eb'
+else
+    # Assume all
+    macaddr='dc:a6:32|b8:27:eb'
+fi
+sudo nmap -n -sP ${prefix}.* | egrep -i "$macaddr" -B2 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'
